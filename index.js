c35c6489f7220832cbe04a0f9470d965a6d548f7
Shiv <shiviyer55x@gmail.com>
2023-04-26 17:27:57 +0800

    initial sanity test worked

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ const wax = require('wax-on');
 require("dotenv").config();
 
 // create an instance of Express app
+let app = express();
+
+// Set the view engine to be Handlebars
+app.set("view engine", "hbs");
+
+// static folder
+app.use(express.static("public"));
+
+// setup wax-on
+wax.on(hbs.handlebars);
+wax.setLayoutPath("./views/layouts");
+
+// enable forms
+app.use(
+    express.urlencoded({
+        extended: false
+    })
+);
+
+// main function
+async function main() {
+    app.get("/", (req, res) => {
+        res.send("sup");
+    });
+}
+
+main();
+
+app.listen(3000, () => {
+    console.log("Server has started WOO");
+});
