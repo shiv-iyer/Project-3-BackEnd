a692319ae1aa2cddc504019897bf5c114e1247fb
Shiv <shiviyer55x@gmail.com>
2023-04-26 17:18:05 +0800

    hello world

---
+++ index.js
@@ -0,0 +1,7 @@
+// preemptive requirements
+const express = require('express');
+const hbs = require("hbs");
+const wax = require('wax-on');
+require("dotenv").config();
+
+// create an instance of Express app
