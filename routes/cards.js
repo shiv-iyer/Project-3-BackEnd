23c343183f3cd3ed2b3e25b6e6ef4f18cfa5d374
Shiv <shiviyer55x@gmail.com>
2023-04-26 17:34:33 +0800

    base framework completed shoutout isaac

--- routes/cards.js
+++ routes/cards.js
@@ -1,1 +1,13 @@
+// get Express
 const express = require("express");
+// use the Express Router
+const router = express.Router();
+
+// res.render will automatically go to the views folder,
+// because we set the view engine to handlebars, we don't need to write index.hbs, just index
+router.get("/", (req, res) => {
+    res.render("cards/index");
+});
+
+// export the Router out
+module.exports = router;
