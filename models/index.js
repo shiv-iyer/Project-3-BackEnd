acbafdbe4e3db2a2b76ef0ba1e8537faf37d2804
Shiv <shiviyer55x@gmail.com>
2023-05-01 14:03:20 +0800

    relationshep via model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 // A Model is a JavaScript class that represents one table. One instance of said model represents one row in the table.
+// All models will go in index.js
 
 // Use Bookshelf: from our previously written Bookshelf code
 const bookshelf = require('../bookshelf');
@@ -6,7 +7,23 @@ const bookshelf = require('../bookshelf');
 // Create a Card Model using Bookshelf, and store it in the Card object
 const Card = bookshelf.model('Card', {
     // the cards table from the pokeport database
-    tableName: 'cards'
+    tableName: 'cards',
+    expansion() {
+        // referring to the expansion model
+        // if not following the naming convention, also include the FK name
+        return this.belongsTo("Expansion", "expansion_id")
+    }
+});
+
+// Expansion Model
+const Expansion = bookshelf.model('Expansion', {
+    tableName: 'expansions',
+    // define the relationship, models refer to other models
+    card() {
+        // referring to the card model
+        return this.hasMany("Card");
+    },
+
 });
 
 // export the Card object that stores the Card model out
