4f14690c26a58b8c1531f45e44bb679f46f76b2a
shiv-iyer <shiviyer55x@gmail.com>
2023-05-03 07:55:01 +0000

    user login and authentication done! yay

--- forms/index.js
+++ forms/index.js
@@ -198,4 +198,23 @@ const createRegistrationForm = () => {
     })
 };
 
-module.exports = { createCardForm, createRegistrationForm, bootstrapField };
+const createLoginForm = () => {
+    return forms.create({
+        'email': fields.string({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        }),
+        'password': fields.password({
+            required: true,
+            errorAfterField: true,
+            cssClasses: {
+                label: ['form-label']
+            }
+        })
+    });
+};
+
+module.exports = { createCardForm, createRegistrationForm, createLoginForm, bootstrapField };
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const router = express.Router();
 const { User } = require("../models");
 
 // import in the form stuff
-const { createRegistrationForm, bootstrapField } = require("../forms");
+const { createRegistrationForm, createLoginForm, bootstrapField } = require("../forms");
 const { create } = require("hbs");
 
 // route to create the registration form
@@ -47,7 +47,82 @@ router.post("/register", (req, res) => {
 
 // route for user login page
 router.get("/login", (req, res) => {
-    res.render("users/login");
-})
+    // create the login form
+    const loginForm = createLoginForm();
+    res.render("users/login", {
+        'form': loginForm.toHTML(bootstrapField)
+    });
+});
+
+// route for processing user login
+router.post("/login", (req, res) => {
+    const loginForm = createLoginForm();
+    loginForm.handle(req, {
+        'success': async (form) => {
+            // process login
+
+            // find user by email
+            let user = await User.where({
+                // match the form fields
+                'email': form.data.email
+            }).fetch({
+                require: false
+            });
+
+            // if no user was found with the provided email, throw an error
+            if (!user){
+                req.flash("error_messages", "Sorry, but there was no user found with the specified email address. Please try again.");
+                res.redirect("/users/login");
+            } else {
+                // if user found, check if the password details match
+                if (user.get('password') === form.data.password) {
+                    // store user details in the session
+                    const username = user.get('username');
+                    req.session.user = {
+                        id: user.get('id'),
+                        username: username,
+                        email: user.get('email')
+                    };
+                    req.flash("success_messages", "Welcome back, " + username + "!");
+                    res.redirect("/users/profile");
+                }  else {
+                    req.flash("error_messages", "Sorry, but the password you provided was incorrect. Please try again.");
+                    res.redirect("/users/login");
+                };
+            }
+        },
+        'error': (form) => {
+            req.flash("error_messages", "Sorry, there were some errors logging you in. Please try filling the form out again.");
+            res.render("users/login", {
+                'form': form.toHTML(bootstrapField)
+            });
+        }
+    })
+});
+
+// route for user profile
+router.get("/profile", (req, res) => {
+    // get user from the current session
+    const user = req.session.user;
+    // if no user exists... don't load the page, and redirect to the login page
+    if (!user) {
+        req.flash("error_messages", "Sorry, you do not have permission to view this page. Please log in.");
+        res.redirect("/users/login");
+    } else {
+        // otherwise, load the profile with the user
+        res.render("users/profile", {
+            'user': user
+        });
+    };
+});
+
+// logout route
+router.get("/logout", (req, res) => {
+    // terminate the user in the session (set to null)
+    req.session.user = null;
+    req.flash("success_messages", "Goodbye, come again soon!");
+    res.redirect("/users/login");
+});
+
 
 module.exports = router;
