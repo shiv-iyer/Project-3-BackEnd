cd0b00a7fae5bfb23aca0a5ad9a817d4bd6c8b98
shiv-iyer <shiviyer55x@gmail.com>
2023-05-08 06:57:37 +0000

    shopping cart doneeee

--- routes/shoppingCart.js
+++ routes/shoppingCart.js
@@ -16,11 +16,28 @@ router.get("/", checkIfAuthenticated, async (req, res) => {
     });
 });
 
+// add to shopping cart
 router.get("/:card_id/add", checkIfAuthenticated, async (req, res) => {
     let cart = new CartServices(req.session.user.id);
     await cart.addToCart(req.params.card_id, 1);
     req.flash("success_messages", "Yay! Item was successfully added to your cart!");
     res.redirect("/cards");
+});
+
+// remove from shopping cart
+router.get("/:card_id/remove", checkIfAuthenticated, async (req, res) => {
+    let cart = new CartServices(req.session.user.id);
+    await cart.removeFromCart(req.params.card_id);
+    req.flash("success_messages", "Yay! Item was successfully removed from your cart!");
+    res.redirect("/cart");
+});
+
+// update existing cart item's quantity
+router.post("/:card_id/quantity/update", async (req, res) => {
+    let cart = new CartServices(req.session.user.id);
+    await cart.setQuantity(req.params.card_id, req.body.newQuantity);
+    req.flash("success_messages", "Yay! Cart item's quantity was successfully updated!");
+    res.redirect("/cart");
 });
 
 module.exports = router;
