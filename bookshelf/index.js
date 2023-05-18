2a32b3db755e06eb38dca3a59d85858146fdb4f8
shiv-iyer <shiviyer55x@gmail.com>
2023-05-18 10:55:47 +0000

    deploy

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -2,14 +2,15 @@
 // We use it for: independence from SQL syntax (or any other database) — our code will work with all kinds of SQL databases
 // also, most ORM solutions, such as Bookshelf, have some basic security included.
 
-// Set up the database connection; details should match those in database.json
+// modified to deploy
 const knex = require('knex')({
-    client: process.env.DB_DRIVER,
-    connection: {
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        host: process.env.DB_HOST
+    'client':process.env.DB_DRIVER,
+    'connection':{
+        'user': process.env.DB_USER,
+        'password':process.env.DB_PASSWORD,
+        'database':process.env.DB_DATABASE,
+        'host': process.env.DB_HOST,
+        'ssl': true
     }
 });
 
